aeb3959e5f68e526340ca0294fa419a79603c7e1
PiotrPucolowski <93589319+PiotrPucolowski@users.noreply.github.com>
2021-11-05 13:47:51 +0100

    Add files via upload
    przeciagniete

---
+++ afirstprogram/Main01.java
@@ -0,0 +1,9 @@
+package pl.coderslab.afirstprogram;
+
+public class Main01 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ afirstprogram/Main02.java
@@ -0,0 +1,10 @@
+package pl.coderslab.afirstprogram;
+
+public class Main02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello Coderslab");
+    }
+
+}
